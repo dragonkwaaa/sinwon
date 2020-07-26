070e139dc6383b8d69ff92d02220d4d5cc3f46c9
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-07-26 20:49:22 +0900

    2차 회의 이후

--- [save_after_20200714_2214]/setting/pages/headLine.php
+++ [save_after_20200714_2214]/setting/pages/headLine.php
@@ -3,7 +3,7 @@
 	<a href="/sinwon/setting/corpList" class="btn subPartMod <?=($subCode == '002' ? ' activated' : '')?>">거래처관리</a>
 	<a href="/sinwon/setting/paymentList" class="btn subPartMod <?=($subCode == '003' ? ' activated' : '')?>">결제수단관리</a>
 	<!-- <a href="/sinwon/setting/payTypeList" class="btn subPartMod <?=($subCode == '004' ? ' activated' : '')?>">지급수단관리</a> -->
-	<a href="/sinwon/setting/outcomeList" class="btn subPartMod <?=($subCode == '005' ? ' activated' : '')?>">고정지출관리</a>
+	<!-- <a href="/sinwon/setting/outcomeList" class="btn subPartMod <?=($subCode == '005' ? ' activated' : '')?>">고정지출관리</a> -->
 	<!-- <a href="/sinwon/setting/branchList" class="btn subPartMod <?=($subCode == '006' ? ' activated' : '')?>">지사관리</a>
 	<a href="/sinwon/setting/agencyList" class="btn subPartMod <?=($subCode == '007' ? ' activated' : '')?>">대리점관리</a> -->
     <a href="/sinwon/setting/staffInfo" class="btn subPartMod <?=($subCode == '008' ? ' activated' : '')?>">직원관리</a>
